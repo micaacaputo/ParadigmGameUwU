46fe6aa6bbd86eb954f97c3105ddd4c3024d45b1
Facu-SH <fundacosh@gmail.com>
2023-11-28 17:29:00 -0300

    Pruebas Unitarias

--- Entrega 1/Vector2.cs
+++ Entrega 1/Vector2.cs
@@ -21,5 +21,10 @@
             return !(a==b);
         }
 
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x + b.x, a.y + b.y);
+        }
+
     }
 }
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyGame.assets;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Vector2Tests
+    {
+        [TestMethod]
+        public void EqualityTest()
+        {
+            // Vectors
+            Vector2 vectorA = new Vector2(1.0f, 2.0f);
+            Vector2 vectorB = new Vector2(1.0f, 2.0f);
+            Vector2 vectorC = new Vector2(3.0f, 4.0f);
+
+            // Results
+            Assert.IsTrue(vectorA == vectorB);
+            Assert.IsFalse(vectorA == vectorC);
+        }
+
+        [TestMethod]
+        public void InequalityTest()
+        {
+            // Vectors
+            Vector2 vectorA = new Vector2(1.0f, 2.0f);
+            Vector2 vectorB = new Vector2(1.0f, 2.0f);
+            Vector2 vectorC = new Vector2(3.0f, 4.0f);
+
+            // Results
+            Assert.IsFalse(vectorA != vectorB);
+            Assert.IsTrue(vectorA != vectorC);
+        }
+        [TestMethod]
+        public void SumTest()
+        {
+            // Vectors
+            Vector2 vectorA = new Vector2(1.0f, 2.0f);
+            Vector2 vectorB = new Vector2(3.0f, 4.0f);
+
+            // Sum
+            Vector2 result = vectorA + vectorB;
+
+            // Results
+            Assert.AreEqual(4.0f, result.x);
+            Assert.AreEqual(6.0f, result.y);
+        }
+    }
+}
